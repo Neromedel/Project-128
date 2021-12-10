99ecb6a9f8648e5013f3a8a8b8e2339538fe4787
Neromedel <79370397+Neromedel@users.noreply.github.com>
2021-12-10 16:52:05 +0530

    dd

---
+++ main.js
@@ -0,0 +1,45 @@
+song_1 ="";
+song_2 ="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload(){
+song_1 =loadSound("Cradles.mp3");
+song_2 =loadSound("Love Nwantiti.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.position(30,40);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    song_1.play();
+}
+
+function modelLoaded(){
+    console.log("Modle is intialized.");
+    poseNet.on('pose',gotPoses);
+}
+
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Left Wrist X: "+leftWristX+"  Left Wrist Y: "+leftWristY);
+        console.log("Right Wrist X: "+rightWristX+"  Right Wrist Y: "+rightWristY);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+
+
